a3a5caa3dd276a638fa5c025750a0e8b618cd48a
VithalAutade <vithalautade99@gmail.com>
2022-06-16 14:08:39 +0530

    ss

--- src/app/Modules/admin/admin-routing.module.ts
+++ src/app/Modules/admin/admin-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { OtRatesComponent } from './ot-rates/ot-rates.component';
+import { OtratesformComponent } from './otratesform/otratesform.component';
+import { TaxTableComponent } from './tax-table/tax-table.component';
 
-const routes: Routes = [{ path: '', component: AdminComponent }];
+const routes: Routes = [
+  { path: '', component: AdminComponent },
+  { path: 'OtRates',component: OtRatesComponent},
+  { path: 'TaxTable',component: TaxTableComponent},
+  { path: 'Otratesform',component: OtratesformComponent}
+
+
+
+
+
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/Modules/admin/admin.module.ts
+++ src/app/Modules/admin/admin.module.ts
@@ -4,11 +4,17 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { SharedModule } from '../shared/shared.module';
+import { OtRatesComponent } from './ot-rates/ot-rates.component';
+import { TaxTableComponent } from './tax-table/tax-table.component';
+import { OtratesformComponent } from './otratesform/otratesform.component';
 
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    OtRatesComponent,
+    TaxTableComponent,
+    OtratesformComponent
   ],
   imports: [
     CommonModule,
