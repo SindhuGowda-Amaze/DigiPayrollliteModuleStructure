318025a5046b3b23be90b7f3ef2f0978e2385273
Divya <divyashree@amazeinc.in>
2022-06-17 12:54:24 +0530

    ddd

--- src/app/Modules/hr/bank-advice-list/bank-advice-list.component.ts
+++ src/app/Modules/hr/bank-advice-list/bank-advice-list.component.ts
@@ -24,7 +24,13 @@ export class BankAdviceListComponent implements OnInit {
   loader: any;
   ComputationBasic: any;
   ComputationDeminimis: any;
-  currentUrl:any;
+  currentUrl: any;
+  selectrun: any;
+  thirteenthrunpayroll2: any;
+  month: any;
+  finallist: any;
+  uniquelist12: any;
+  fileName = 'Bank Advice List.xlsx';
 
   ngOnInit(): void {
     this.currentUrl = window.location.href;
@@ -34,39 +40,30 @@ export class BankAdviceListComponent implements OnInit {
     this.loader = true;
   }
 
-public GetCompany_PayrollComputation(){
-  this.DigiofficeService.GetCompany_PayrollComputation()
-  .subscribe(
-    data => {
-      debugger
-      this.result = data;
-
-      this.ComputationBasic = this.result[0].computationBasic,
-        this.ComputationDeminimis = this.result[0].computationDeminimis
-
-
-    }
-  )
-  // .subscribe({
-  //   next: data => {
-  //     debugger
-  //     this.Citylist = data;
-  //     this.loader=false;
-  //   }, error: (err) => {
-  //     Swal.fire('Issue in Getting City Type');
-  //     // Insert error in Db Here//
-  //     var obj = {
-  //       'PageName': this.currentUrl,
-  //       'ErrorMessage': err.error.message
-  //     }
-  //     this.DigiofficeService.InsertExceptionLogs(obj).subscribe(
-  //       data => {
-  //         debugger
-  //       },
-  //     )
-  //   }
-  // })
-}
+  public GetCompany_PayrollComputation() {
+    this.DigiofficeService.GetCompany_PayrollComputation()
+      .subscribe({
+        next: data => {
+          debugger
+          this.result = data;
+          this.ComputationBasic = this.result[0].computationBasic,
+            this.ComputationDeminimis = this.result[0].computationDeminimis
+          this.loader = false;
+        }, error: (err) => {
+          Swal.fire('Issue in Getting Company Payroll Computation');
+          // Insert error in Db Here//
+          var obj = {
+            'PageName': this.currentUrl,
+            'ErrorMessage': err.error.message
+          }
+          this.DigiofficeService.InsertExceptionLogs(obj).subscribe(
+            data => {
+              debugger
+            },
+          )
+        }
+      })
+  }
 
   public Getyear(event: any) {
     debugger
@@ -82,8 +79,7 @@ public GetCompany_PayrollComputation(){
     debugger
     this.PayrollType = event.target.value;
   }
-  selectrun: any;
-  thirteenthrunpayroll2: any;
+
   public runpayroll() {
     this.selectrun = 1
     this.thirteenthrunpayroll2 = 0
@@ -106,24 +102,32 @@ public GetCompany_PayrollComputation(){
       if (ele != null) {
         ele.click();
       }
-      this.DigiofficeService.GetEmployeeSalary().subscribe(data => {
-        debugger
-        this.employeelist = data.filter(x => x.startyear == this.year && x.month == this.Month && x.payrolltype == this.PayrollType);
-        const key = 'startdate';
-
-        this.uniquelist = [...new Map(this.employeelist.map((item: { [x: string]: any; }) =>
-          [item[key], item])).values()];
-        //  this.uniquelist = [...new Set(data.map(item => item))];
-        this.loader = false
-      });
+      this.DigiofficeService.GetEmployeeSalary()
+        .subscribe({
+          next: data => {
+            debugger
+            this.employeelist = data.filter(x => x.startyear == this.year && x.month == this.Month && x.payrolltype == this.PayrollType);
+            const key = 'startdate';
+            this.uniquelist = [...new Map(this.employeelist.map((item: { [x: string]: any; }) =>
+              [item[key], item])).values()];
+            this.loader = false;
+          }, error: (err) => {
+            Swal.fire('Issue in Getting Employee Salary');
+            // Insert error in Db Here//
+            var obj = {
+              'PageName': this.currentUrl,
+              'ErrorMessage': err.error.message
+            }
+            this.DigiofficeService.InsertExceptionLogs(obj).subscribe(
+              data => {
+                debugger
+              },
+            )
+          }
+        })
     }
-
   }
 
-
-  month: any;
-  finallist: any;
-  uniquelist12: any;
   public getthirteenthbankadvicelist() {
     debugger;
     if (this.Month == " " || this.Month == undefined) {
@@ -135,30 +139,36 @@ public GetCompany_PayrollComputation(){
       if (ele != null) {
         ele.click();
       }
-      this.DigiofficeService.GetEmployeeFinalSalary().subscribe(data => {
-        debugger
-        this.finallist = data.filter(x => x.month == this.Month && x.year == this.year);
-        const key = 'staffID';
-
-        this.uniquelist12 = [...new Map(this.finallist.map((item: { [x: string]: any; }) =>
-          [item[key], item])).values()];
-
-        this.loader = false;
-
-        //  this.uniquelist = [...new Set(data.map(item => item))];
-
-
-      });
+      this.DigiofficeService.GetEmployeeFinalSalary()
+        .subscribe({
+          next: data => {
+            debugger
+            this.finallist = data.filter(x => x.month == this.Month && x.year == this.year);
+            const key = 'staffID';
+            this.uniquelist12 = [...new Map(this.finallist.map((item: { [x: string]: any; }) =>
+              [item[key], item])).values()];
+            this.loader = false;
+          }, error: (err) => {
+            Swal.fire('Issue in Getting Employee Final Salary');
+            // Insert error in Db Here//
+            var obj = {
+              'PageName': this.currentUrl,
+              'ErrorMessage': err.error.message
+            }
+            this.DigiofficeService.InsertExceptionLogs(obj).subscribe(
+              data => {
+                debugger
+              },
+            )
+          }
+        })
     }
-
   }
 
   public convetToPDF1() {
     debugger
-
     var data: any = document.getElementById('downloadaplication');
     html2canvas(data).then(canvas => {
-
       var margin = 5;
       var imgWidth = 208
       // var pageHeight = 295 - 10 * margin;
@@ -168,55 +178,36 @@ public GetCompany_PayrollComputation(){
       var doc = new jsPDF('p', 'mm');
       var position = 0;
       while (heightLeft > 0) {
-
         const contentDataURL = canvas.toDataURL('image/png')
         position = heightLeft - imgHeight;
-
         doc.addPage();
-
-
         doc.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
-
         heightLeft -= pageHeight;
-
       }
       doc.deletePage(1)
       doc.save('Bank Advice List.pdf');
-
       var pdf1 = doc.output('blob');
       var file = new File([pdf1], "Application.pdf");
       let body = new FormData();
       debugger
       body.append('Dan', file);
       console.log('pdf', pdf1)
-
-
-
     }).then(() => {
-
     });;
   }
 
-
-
-  fileName = 'Bank Advice List.xlsx';
   exportexcel1(): void {
     /* table id is passed over here */
     let element = document.getElementById('downloadaplication');
     debugger
     const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
     debugger
-
     /* generate workbook and add the worksheet */
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-
     /* save to file */
     XLSX.writeFile(wb, this.fileName);
-
   }
-
-
 
   exportexcel(): void {
     /* table id is passed over here */
@@ -224,15 +215,11 @@ public GetCompany_PayrollComputation(){
     debugger
     const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
     debugger
-
     /* generate workbook and add the worksheet */
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-
     /* save to file */
     XLSX.writeFile(wb, this.fileName);
-
   }
-
 
 }
